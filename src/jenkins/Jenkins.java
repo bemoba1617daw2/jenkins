bcc812f2e71d80203023c7a62ac689c24b169764
Gobi <Gobi@172.20.9.60>
2017-02-21 16:49:37 +0100

    Modifiacio IC jenkins

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jenkins;
 import java.io.*;
 /**
@@ -25,6 +20,7 @@ public class Jenkins {
             bw.write("</head>");bw.newLine();
             bw.write("<body>");bw.newLine();
             bw.write("Projecte per provar l'IC Jenkins");bw.newLine();
+            bw.write(" Modificacio del projecte jenkins");bw.newLine();
             bw.write("</body>");bw.newLine();
             bw.write("</html>");bw.newLine();
             bw.close();
